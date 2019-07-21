f0b2f94f21548934c0e0469d516d8de0e2eb59e7
Thomas Caycedo <thomascaycedo19@outlook.com>
2019-07-20 21:11:32 -0500

    Add project files.

---
+++ Proyecto3/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto3.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void Subir(HttpPostedFileBase file)
+        {
+            string archivo = (file.FileName).ToLower();
+
+            try
+            {
+                file.SaveAs(Server.MapPath("~/Uploads/" + archivo));
+            }
+            catch (Exception e)
+            {
+               
+            }           
+        }
+    }
+}
---
+++ Proyecto3/Controllers/XmlController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HtmlAgilityPack;
+
+namespace Proyecto3.Controllers
+{
+    public class XmlController : Controller
+    {
+        // GET: Xml
+        public ActionResult Transformation()
+        {
+            return View();
+        }
+
+        public string Xml()
+        {
+
+            var path = Server.MapPath("~/Uploads/data.html");
+
+            var doc = new HtmlDocument();
+            doc.Load(path);
+
+            var node = doc.DocumentNode.SelectSingleNode("//body");
+
+            return node.OuterHtml;
+        }
+
+       
+    }
+}
